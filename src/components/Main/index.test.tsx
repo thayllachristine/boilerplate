85326f013dc5f921d16c269e7034ab7884b28090
thaylla <thayllachristineo@gmail.com>
2021-03-18 17:47:09 -0300

    feat: create Main component

---
+++ src/components/Main/index.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the heading', () => {
+    render(<Main />);
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i }),
+    ).toBeInTheDocument();
+  });
+});
